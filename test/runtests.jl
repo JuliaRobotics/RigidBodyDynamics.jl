f09b67ea5e59637c9d904606016e6f0b3f29b0e9
Twan Koolen <koolen.twan@gmail.com>
2016-04-20 16:41:15 -0400

    test notebooks

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,8 @@ using Base.Test
 using Quaternions
 using FixedSizeArrays
 import RigidBodyDynamics
+
+for f in filter(x -> endswith(x, "ipynb"), readdir("../examples"))
+    notebook = "../examples/" * f
+    run(`jupyter nbconvert --to notebook --execute $notebook --output $notebook`)
+end
