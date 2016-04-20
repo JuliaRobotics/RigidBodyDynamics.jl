1d22be7f1e2375499bab8d9e796895ce41dc4008
Twan Koolen <koolen.twan@gmail.com>
2016-04-20 19:14:17 -0400

    use IJulia's jupyter variable

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Quaternions
 using FixedSizeArrays
 import RigidBodyDynamics
 
-import Conda
-condaDir = Conda.SCRIPTDIR
+using IJulia
+jupyter = IJulia.jupyter
 for f in filter(x -> endswith(x, "ipynb"), readdir("../examples"))
     notebook = "../examples/" * f
-    run(`export PATH=$condaDir:\$PATH && jupyter nbconvert --to notebook --execute $notebook --output $notebook`)
+    run("$jupyter nbconvert --to notebook --execute $notebook --output $notebook")
 end
