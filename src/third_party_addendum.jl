ead16e32703124df997171f48df33430d1d4c37c
Twan Koolen <koolen.twan@gmail.com>
2016-05-19 14:24:29 -0400

    use own implementation of angle axis (copied from Quaternions.jl master) so as to provide a drop in replacement

--- src/third_party_addendum.jl
+++ src/third_party_addendum.jl
@@ -13,3 +13,15 @@ function isapprox_tol{FSA <: FixedArray, A <: Union{Array, FixedArray}}(a::FSA, 
     end
     true
 end
+
+angle_axis_proper(q::Quaternion) = angle_proper(q), axis_proper(q)
+
+angle_proper(q::Quaternion) = 2 * acos(real(Quaternions.normalize(q)))
+
+function axis_proper(q::Quaternion)
+    q = Quaternions.normalize(q)
+    s = sin(angle(q) / 2)
+        abs(s) > 0 ?
+        [q.v1, q.v2, q.v3] / s :
+        [1.0, 0.0, 0.0]
+end
