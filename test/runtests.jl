77f17a01bbe7727ea2d0c63096a75aa344a8e8b4
Twan Koolen <koolen.twan@gmail.com>
2016-08-03 02:38:49 -0400

    disable running notebooks for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,8 +18,8 @@ include("test_double_pendulum.jl")
 include("test_mechanism.jl")
 
 # run notebooks
-jupyter = IJulia.jupyter
-for f in filter(x -> endswith(x, "ipynb"), readdir("../examples"))
-    notebook = "../examples/" * f
-    run(`$jupyter nbconvert --to notebook --execute $notebook --output $notebook`)
-end
+# jupyter = IJulia.jupyter
+# for f in filter(x -> endswith(x, "ipynb"), readdir("../examples"))
+#     notebook = "../examples/" * f
+#     run(`$jupyter nbconvert --to notebook --execute $notebook --output $notebook`)
+# end
