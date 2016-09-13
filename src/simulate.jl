a42f6881d8cffde39a189950fffa374becf7aa3e
Twan Koolen <koolen.twan@gmail.com>
2016-09-13 16:43:08 -0400

    add simulate, require ODE

---
+++ src/simulate.jl
@@ -0,0 +1,10 @@
+function simulate(state0::MechanismState, tspan; integrator = ode45, kwargs...)
+    q0 = configuration_vector(state0)
+    v0 = velocity_vector(state0)
+    x0 = [q0; v0]
+    T = cache_eltype(state0)
+    state = state0
+    result = DynamicsResult(T, state.mechanism)
+    odefun(t, x) = dynamics!(result, state, x)
+    times, states = integrator(odefun, x0, tspan; kwargs...)
+end
