06714e111606966d13504a4ff18eeb96b5f203fc
Twan Koolen <koolen.twan@gmail.com>
2017-01-13 14:12:42 -0500

    extract out smatrix3x6view, to be used for constraint wrench subspaces as well

--- src/util.jl
+++ src/util.jl
@@ -53,6 +53,26 @@ Base.setindex!(v::UnsafeVectorView, value, idx) = unsafe_store!(v.ptr, value, id
 Base.length(v::UnsafeVectorView) = v.len
 Base.linearindexing{T}(::Type{UnsafeVectorView{T}}) = Base.LinearFast()
 
+# Functions such as motion_subspace(::Joint, ...) need to return types with a number of columns that depends on the joint type.
+# Using a 'view' of a 3×6 SMatrix as the underlying data type gets around type instabilities in motion_subspace while still using an isbits type.
+# See https://github.com/tkoolen/RigidBodyDynamics.jl/issues/84.
+@generated function smatrix3x6view{N, T}(mat::SMatrix{3, N, T})
+    colrange = 1 : N
+    expr = if N == 0
+        :(data = fill(NaN, SMatrix{3, 6, T, 18}))
+    elseif N == 6
+        :(data = mat)
+    else
+        fillerSize = 6 - N
+        filler = fill(NaN, SMatrix{3, fillerSize, T, 3 * fillerSize})
+        :(data = hcat(mat, $filler)::SMatrix{3, 6, T, 18})
+    end
+    return quote
+        $expr
+        ContiguousSMatrixColumnView{3, 6, T, 18}(data, (:, $colrange), 0, 1) # faster way to create view
+    end
+end
+
 const module_tempdir = joinpath(Base.tempdir(), string(module_name(current_module())))
 
 function cached_download(url::String, localFileName::String, cacheDir::String = joinpath(module_tempdir, string(hash(url))))
