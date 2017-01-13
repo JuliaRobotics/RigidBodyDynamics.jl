a5ed8ebdce85272ac65e0fe15f5e06274ee1ee8d
Twan Koolen <koolen.twan@gmail.com>
2017-01-13 14:12:42 -0500

    extract out smatrix3x6view, to be used for constraint wrench subspaces as well

--- src/spatial.jl
+++ src/spatial.jl
@@ -379,37 +379,10 @@ function GeometricJacobian{A<:AbstractMatrix}(body::CartesianFrame3D, base::Cart
     GeometricJacobian{A}(body, base, frame, angular, linear)
 end
 
-# MotionSubspace is the return type of the motion_subspace(::Joint, ...) method. Defining it as a
-# GeometricJacobian with a view of a 3×6 SMatrix as the underlying data type gets around type
-# instabilities in motion_subspace while still using an isbits type.
-# See https://github.com/tkoolen/RigidBodyDynamics.jl/issues/84.
-typealias MotionSubspaceArrayType{T} ContiguousSMatrixColumnView{3, 6, T, 18}
-typealias MotionSubspace{T} GeometricJacobian{MotionSubspaceArrayType{T}}
-@inline function fast_view_for_motion_subspace{T}(data::SMatrix{3, 6, T, 18}, colrange::UnitRange{Int64})
-    MotionSubspaceArrayType{T}(data, (:, colrange), 0, 1)
-end
-
-@generated function MotionSubspace{N, T}(body::CartesianFrame3D, base::CartesianFrame3D, frame::CartesianFrame3D, angular::SMatrix{3, N, T}, linear::SMatrix{3, N, T})
-    colrange = 1 : N
-    if N == 0
-        angularView = fast_view_for_motion_subspace(fill(NaN, SMatrix{3, 6, T, 18}), colrange)
-        linearView = fast_view_for_motion_subspace(fill(NaN, SMatrix{3, 6, T, 18}), colrange)
-        return quote
-            MotionSubspace{T}(body, base, frame, $angularView, $linearView)
-        end
-    elseif N == 6
-        return quote
-            MotionSubspace{T}(body, base, frame, fast_view_for_motion_subspace(angular, $colrange), fast_view_for_motion_subspace(linear, $colrange))
-        end
-    else
-        fillerSize = 6 - N
-        filler = fill(NaN, SMatrix{3, fillerSize, T, 3 * fillerSize})
-        return quote
-            angularData = hcat(angular, $filler)::SMatrix{3, 6, T, 18}
-            linearData = hcat(linear, $filler)::SMatrix{3, 6, T, 18}
-            MotionSubspace{T}(body, base, frame, fast_view_for_motion_subspace(angularData, $colrange), fast_view_for_motion_subspace(linearData, $colrange))
-        end
-    end
+# MotionSubspace is the return type of motion_subspace(::Joint, ...)
+typealias MotionSubspace{T} GeometricJacobian{ContiguousSMatrixColumnView{3, 6, T, 18}}
+function MotionSubspace(body::CartesianFrame3D, base::CartesianFrame3D, frame::CartesianFrame3D, angular, linear)
+    GeometricJacobian(body, base, frame, smatrix3x6view(angular), smatrix3x6view(linear))
 end
 
 convert{A}(::Type{GeometricJacobian{A}}, jac::GeometricJacobian{A}) = jac
