f4f68b1728be6a5ae9529c9c5d0874b729d16ac5
Twan Koolen <koolen.twan@gmail.com>
2017-03-13 11:33:27 -0400

    Start using new graph types in Mechanism.

--- src/RigidBodyDynamics.jl
+++ src/RigidBodyDynamics.jl
@@ -30,13 +30,13 @@ include("cache_element.jl")
 
 importall .TreeDataStructure
 include("mechanism.jl")
-include("mechanism_manipulation.jl")
-include("mechanism_state.jl")
-include("dynamics_result.jl")
-include("mechanism_algorithms.jl")
-include("parse_urdf.jl")
-include("ode_integrators.jl")
-include("simulate.jl")
+# include("mechanism_manipulation.jl")
+# include("mechanism_state.jl")
+# include("dynamics_result.jl")
+# include("mechanism_algorithms.jl")
+# include("parse_urdf.jl")
+# include("ode_integrators.jl")
+# include("simulate.jl")
 
 export
     # types
--- src/mechanism.jl
+++ src/mechanism.jl
@@ -1,9 +1,3 @@
-type NonTreeEdge{T}
-    joint::Joint{T}
-    predecessor::RigidBody{T}
-    successor::RigidBody{T}
-end
-
 """
 $(TYPEDEF)
 
@@ -12,15 +6,16 @@ A `Mechanism` represents an interconnection of rigid bodies and joints.
 state-dependent information.
 """
 type Mechanism{T<:Number}
-    toposortedTree::Vector{TreeVertex{RigidBody{T}, Joint{T}}} # TODO: consider replacing with just the root vertex after creating iterator
-    nonTreeEdges::Vector{NonTreeEdge{T}}
+    graph::DirectedGraph{RigidBody{T}, Joint{T}}
+    tree::SpanningTree{RigidBody{T}, Joint{T}}
     gravitationalAcceleration::FreeVector3D{SVector{3, T}} # TODO: consider removing
 
     function (::Type{Mechanism{T}}){T<:Number}(rootBody::RigidBody{T}; gravity::SVector{3, T} = SVector(zero(T), zero(T), T(-9.81)))
-        tree = Tree{RigidBody{T}, Joint{T}}(rootBody)
-        nonTreeEdges = Vector{NonTreeEdge{T}}()
+        graph = DirectedGraph{RigidBody{T}, Joint{T}}()
+        add_vertex!(graph, rootBody)
+        tree = SpanningTree(graph, rootBody)
         gravitationalAcceleration = FreeVector3D(default_frame(rootBody), gravity)
-        new{T}(toposort(tree), nonTreeEdges, gravitationalAcceleration)
+        new{T}(graph, tree, gravitationalAcceleration)
     end
 end
 
@@ -32,16 +27,35 @@ be attached with joints.
 """
 Mechanism{T}(rootBody::RigidBody{T}; kwargs...) = Mechanism{T}(rootBody; kwargs...)
 Base.eltype{T}(::Mechanism{T}) = T
-root_vertex(mechanism::Mechanism) = mechanism.toposortedTree[1]
-non_root_vertices(mechanism::Mechanism) = view(mechanism.toposortedTree, 2 : length(mechanism.toposortedTree)) # TODO: allocates
-tree(mechanism::Mechanism) = mechanism.toposortedTree[1]
+# tree(mechanism::Mechanism) = mechanism.tree
+
+"""
+$(SIGNATURES)
+
+Return the `Joint`s that are part of the `Mechanism` as an iterable collection.
+"""
+joints(mechanism::Mechanism) = edges(mechanism.graph)
+
+"""
+$(SIGNATURES)
+
+Return the `Joint`s that are part of the `Mechanism`'s spanning tree as an iterable collection.
+"""
+tree_joints(mechanism::Mechanism) = edges(mechanism.tree)
+
+"""
+$(SIGNATURES)
+
+Return the `RigidBody`s that are part of the `Mechanism` as an iterable collection.
+"""
+bodies{T}(mechanism::Mechanism{T}) = vertices(mechanism.graph)
 
 """
 $(SIGNATURES)
 
 Return the root (stationary 'world') body of the `Mechanism`.
 """
-root_body(mechanism::Mechanism) = vertex_data(root_vertex(mechanism))
+root_body(mechanism::Mechanism) = first(bodies(mechanism))
 
 """
 $(SIGNATURES)
@@ -50,57 +64,43 @@ Return the default frame of the root body.
 """
 root_frame(mechanism::Mechanism) = default_frame(root_body(mechanism))
 
-"""
-$(SIGNATURES)
-
-Return the path from rigid body `from` to `to` along edges of the `Mechanism`'s
-kinematic tree.
-"""
-path(mechanism::Mechanism, from::RigidBody, to::RigidBody) = path(findfirst(tree(mechanism), from), findfirst(tree(mechanism), to))
-Base.show(io::IO, mechanism::Mechanism) = print(io, tree(mechanism))
-isinertial(mechanism::Mechanism, frame::CartesianFrame3D) = is_fixed_to_body(frame, root_body(mechanism))
-isroot{T}(mechanism::Mechanism{T}, b::RigidBody{T}) = b == root_body(mechanism)
-non_root_bodies{T}(mechanism::Mechanism{T}) = (vertex_data(vertex) for vertex in non_root_vertices(mechanism))
-num_bodies(mechanism::Mechanism) = length(mechanism.toposortedTree)
-
-"""
-$(SIGNATURES)
-
-Return the `Joint`s that are part of the `Mechanism` as an iterable collection.
-"""
-joints(mechanism::Mechanism) = (edge_to_parent_data(vertex) for vertex in non_root_vertices(mechanism))
-
-"""
-$(SIGNATURES)
-
-Return the `RigidBody`s that are part of the `Mechanism` as an iterable collection.
-"""
-bodies{T}(mechanism::Mechanism{T}) = (vertex_data(vertex) for vertex in mechanism.toposortedTree)
-
+# """
+# $(SIGNATURES)
+#
+# Return the path from rigid body `from` to `to` along edges of the `Mechanism`'s
+# kinematic tree.
+# """
+#TODO: # path(mechanism::Mechanism, from::RigidBody, to::RigidBody) = path(findfirst(tree(mechanism), from), findfirst(tree(mechanism), to))
+#TODO: # Base.show(io::IO, mechanism::Mechanism) = print(io, tree(mechanism))
+Base.@deprecate isroot{T}(mechanism::Mechanism{T}, b::RigidBody{T}) isroot(b, mechanism)
+isroot{T}(b::RigidBody{T}, mechanism::Mechanism{T}) = b == root_body(mechanism)
+non_root_bodies{T}(mechanism::Mechanism{T}) = (body for body in bodies(mechanism) if !isroot(mechanism, body))
+num_bodies(mechanism::Mechanism) = num_vertices(mechanism.graph)
 
 """
 $(SIGNATURES)
 
 Return the dimension of the joint configuration vector ``q``.
 """
-num_positions(mechanism::Mechanism) = num_positions(joints(mechanism))
+num_positions(mechanism::Mechanism) = mapreduce(+, num_positions, 0, tree_joints(mechanism))
 
 """
 $(SIGNATURES)
 
 Return the dimension of the joint velocity vector ``v``.
 """
-num_velocities(mechanism::Mechanism) = num_velocities(joints(mechanism))
+num_velocities(mechanism::Mechanism) = mapreduce(+, num_velocities, 0, tree_joints(mechanism))
 
 """
 $(SIGNATURES)
 
 Return the `RigidBody` to which `frame` is attached.
+
+Note: this function is linear in the number of bodies and is not meant to be
+called in tight loops.
 """
 function body_fixed_frame_to_body(mechanism::Mechanism, frame::CartesianFrame3D)
-    # linear in number of bodies and number of frames; not meant to be super fast
-    for vertex in mechanism.toposortedTree
-        body = vertex_data(vertex)
+    for body in bodies(mechanism)
         if is_fixed_to_body(body, frame)
             return body
         end
@@ -114,6 +114,9 @@ $(SIGNATURES)
 Return the definition of body-fixed frame `frame`, i.e., the `Transform3D` from
 `frame` to the default frame of the body to which it is attached.
 
+Note: this function is linear in the number of bodies and is not meant to be
+called in tight loops.
+
 See also [`default_frame`](@ref), [`frame_definition`](@ref).
 """
 function body_fixed_frame_definition(mechanism::Mechanism, frame::CartesianFrame3D)
@@ -125,6 +128,9 @@ $(SIGNATURES)
 
 Return the transform from `CartesianFrame3D` `from` to `to`, both of which are
 rigidly attached to the same `RigidBody`.
+
+Note: this function is linear in the number of bodies and is not meant to be
+called in tight loops.
 """
 function fixed_transform(mechanism::Mechanism, from::CartesianFrame3D, to::CartesianFrame3D)
     fixed_transform(body_fixed_frame_to_body(mechanism, from), from, to)
@@ -135,18 +141,15 @@ function add_body_fixed_frame!{T}(mechanism::Mechanism{T}, transform::Transform3
     add_frame!(body_fixed_frame_to_body(mechanism, transform.to), transform)
 end
 
-function canonicalize_frame_definitions!{T}(mechanism::Mechanism{T}, vertex::TreeVertex{RigidBody{T}, Joint{T}})
-    if !isroot(vertex)
-        body = vertex_data(vertex)
-        joint = edge_to_parent_data(vertex)
-        parentBody = vertex_data(parent(vertex))
-        newDefaultFrame = joint.frameAfter
-        change_default_frame!(body, newDefaultFrame)
+function canonicalize_frame_definitions!{T}(mechanism::Mechanism{T}, body::RigidBody{T})
+    if !isroot(body, mechanism)
+        joint = edge_to_parent(body, mechanism.tree)
+        change_default_frame!(body, joint.frameAfter)
     end
 end
 
 function canonicalize_frame_definitions!(mechanism::Mechanism)
-    for vertex in non_root_vertices(mechanism)
+    for body in bodies(mechanism)
         canonicalize_frame_definitions!(mechanism, vertex)
     end
 end
@@ -156,17 +159,21 @@ function gravitational_spatial_acceleration(mechanism::Mechanism)
     SpatialAcceleration(frame, frame, frame, zeros(SVector{3, eltype(mechanism)}), mechanism.gravitationalAcceleration.v)
 end
 
+non_tree_joints(mechanism::Mechanism) = setdiff(edges(mechanism.graph), edges(mechanism.tree))
+
 function constraint_jacobian_structure(mechanism::Mechanism)
-    # columns correspond to non-tree edges, rows correspond to tree joints
-    ret = spzeros(Int64, length(mechanism.toposortedTree), length(mechanism.nonTreeEdges))
-    for (col, nonTreeEdge) in enumerate(mechanism.nonTreeEdges)
-        predecessorVertex = findfirst(v -> vertex_data(v) == nonTreeEdge.predecessor, tree(mechanism))
-        successorVertex = findfirst(v -> vertex_data(v) == nonTreeEdge.successor, tree(mechanism))
-        for (vertex, sign) in Dict(successorVertex => 1, predecessorVertex => -1)
-            while !isroot(vertex)
-                row = findfirst(mechanism.toposortedTree, vertex)
-                ret[row, col] += sign
-                vertex = parent(vertex)
+    # TODO: move to MechanismState
+    # columns correspond to non-tree joints, rows correspond to tree joints
+    nonTreeJoints = non_tree_joints(mechanism)
+    ret = spzeros(Int64, num_edges(mechanism.tree), length(nonTreeJoints))
+    for (col, nonTreeJoint) in enumerate(nonTreeJoints)
+        predecessor = source(nonTreeJoint, mechanism.graph)
+        successor = target(nonTreeJoint, mechanism.graph)
+        for (body, sign) in Dict(successor => 1, predecessor => -1)
+            while !isroot(body, mechanism)
+                joint = edge_to_parent(body, mechanism.tree)
+                ret[edge_index(joint), col] += sign
+                body = source(joint)
             end
         end
     end
