02c58847b69a6001d8b2e84fdf1bcba71a4a30c6
Robin Deits <robin.deits@gmail.com>
2017-08-22 16:03:00 -0400

    fix Prismatic() constructor for regular Vector input

--- src/joint_types.jl
+++ src/joint_types.jl
@@ -271,7 +271,7 @@ struct Prismatic{T} <: OneDegreeOfFreedomFixedAxis{T}
     Construct a new `Prismatic` joint type, allowing translation along `axis`
     (expressed in the frame before the joint).
     """
-    Prismatic(axis::AbstractVector{T}) where {T} = new{T}(axis, rotation_between(SVector(zero(T), zero(T), one(T)), SVector(axis)))
+    Prismatic(axis::AbstractVector{T}) where {T} = new{T}(axis, rotation_between(SVector(zero(T), zero(T), one(T)), SVector{3, T}(axis)))
 end
 
 Base.show(io::IO, jt::Prismatic) = print(io, "Prismatic joint with axis $(jt.axis)")
