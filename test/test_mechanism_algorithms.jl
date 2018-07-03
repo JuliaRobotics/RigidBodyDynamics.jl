95d71df206095c8d45b182cd8325d51c42a8126d
Twan Koolen <koolen.twan@gmail.com>
2018-07-03 15:14:21 +0200

    Disable ForwardDiff Hessian test for now (excessive compilation time).

--- test/test_mechanism_algorithms.jl
+++ test/test_mechanism_algorithms.jl
@@ -481,11 +481,12 @@ end
             kinetic_energy(x)
         end
 
-        M2 = similar(M.data)
+        # FIXME: excessive compilation time
+        # M2 = similar(M.data)
         # NOTE: chunk size 1 necessary after updating to ForwardDiff 0.2 because creating a MechanismState with a max size Dual takes forever...
         # see https://github.com/JuliaDiff/ForwardDiff.jl/issues/266
-        M2 = ForwardDiff.hessian!(M2, kinetic_energy_fun, v, ForwardDiff.HessianConfig(kinetic_energy_fun, v, ForwardDiff.Chunk{1}()))
-        @test isapprox(M2, M; atol = 1e-12)
+        # M2 = ForwardDiff.hessian!(M2, kinetic_energy_fun, v, ForwardDiff.HessianConfig(kinetic_energy_fun, v, ForwardDiff.Chunk{1}()))
+        # @test isapprox(M2, M; atol = 1e-12)
     end
 
     @testset "spatial_inertia!" begin
