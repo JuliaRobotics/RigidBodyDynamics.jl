fa08b12515108d63e837c6ea9d9ffc77b8a857ba
Twan Koolen <koolen.twan@gmail.com>
2019-06-28 22:20:26 -0400

    Add copyto, map for CatVector.

--- src/custom_collections/CatVector.jl
+++ src/custom_collections/CatVector.jl
@@ -1,5 +1,5 @@
-struct CatVector{T, N, I<:Tuple{Vararg{AbstractVector{T}, N}}} <: AbstractVector{T}
-    vecs::I
+struct CatVector{T, N, V<:AbstractVector{T}} <: AbstractVector{T}
+    vecs::NTuple{N, V}
 end
 
 @inline Base.size(vec::CatVector) = (mapreduce(length, +, vec.vecs; init=0),)
@@ -39,7 +39,7 @@ Base.@propagate_inbounds function Base.setindex!(vec::CatVector, val, i::Int)
     error()
 end
 
-Base.@propagate_inbounds function Base.copyto!(dest::AbstractVector, src::CatVector)
+Base.@propagate_inbounds function Base.copyto!(dest::AbstractVector{T}, src::CatVector{T}) where {T}
     @boundscheck length(dest) == length(src) || throw(DimensionMismatch())
     dest_indices = eachindex(dest)
     k = 1
@@ -55,3 +55,30 @@ end
 
 Base.similar(vec::CatVector) = CatVector(map(similar, vec.vecs))
 Base.similar(vec::CatVector, ::Type{T}) where {T} = CatVector(map(x -> similar(x, T), vec.vecs))
+
+function check_cat_vectors_line_up(x::CatVector, ys::CatVector...)
+    for j in eachindex(ys)
+        y = ys[j]
+        length(x.vecs) == length(y.vecs) || throw(ArgumentError("Subvectors must line up"))
+        for i in eachindex(x.vecs)
+            length(x.vecs[i]) == length(y.vecs[i]) || throw(ArgumentError("Subvectors must line up"))
+        end
+    end
+end
+
+@inline function Base.copyto!(dest::CatVector, src::CatVector)
+    @boundscheck check_cat_vectors_line_up(dest, src)
+    @inbounds for i in eachindex(dest.vecs)
+        copyto!(dest.vecs[i], src.vecs[i])
+    end
+    return dest
+end
+
+@inline function Base.map!(f::F, dest::CatVector, args::CatVector...) where F
+    @boundscheck check_cat_vectors_line_up(dest, args...)
+    @inbounds for i in eachindex(dest.vecs)
+        map!(f, dest.vecs[i], map(arg -> arg.vecs[i], args)...)
+    end
+    return dest
+end
+
