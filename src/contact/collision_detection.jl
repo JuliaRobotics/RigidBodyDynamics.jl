dab72d0af8c68673c97974fe24fee559598d7e45
Twan Koolen <koolen.twan@gmail.com>
2019-07-16 13:56:48 -0400

    Add HRep, specialize collision checking for point-to-hrep.

--- src/contact/collision_detection.jl
+++ src/contact/collision_detection.jl
@@ -24,7 +24,8 @@ end
 
 function extract_halfspace(result::GJKResult, closest_in_b::SVector{3})
     # IMPORTANT: this only works when the closest face of the simplex corresponds
-    # to the closest face of the Minkowski sum
+    # to the closest face of the Minkowski sum. Should instead use EPA to get the
+    # halfspace and cache it for performance and to avoid tunelling.
     simplex = result.simplex
     best_dist_squared = nothing
     best_face = nothing
@@ -95,4 +96,82 @@ end
 @inline function detect_contact(geometries::Pair{<:HalfSpace, <:GeometryTypes.Point}, pose_a::Transformation, pose_b::Transformation)
     separation, inward_normal, point_in_b, point_in_a = detect_contact(Base.reverse(geometries), pose_b, pose_a)
     separation, -inward_normal, point_in_a, point_in_b
+end
+
+
+struct PointToHRepCollisionCache{F, M, T} # F: whether or not the order of point and hrep is flipped
+    point::GeometryTypes.Point{3, T}
+    hrep::HRep{M, 3, T}
+    penetrated_halfspace_index::Base.RefValue{Int} # 0: uninitialized
+end
+
+PointToHRepCollisionCache(point::GeometryTypes.Point{3, T}, hrep::HRep{M, 3, T}) where {M, T} = PointToHRepCollisionCache{false, M, T}(point, hrep, Ref(0))
+PointToHRepCollisionCache(hrep::HRep{M, 3, T}, point::GeometryTypes.Point{3, T}) where {M, T} = PointToHRepCollisionCache{true, M, T}(point, hrep, Ref(0))
+
+collision_cache(::Type{T}, point::GeometryTypes.Point{3, T}, hrep::HRep{M, 3, T}) where {M, T} = PointToHRepCollisionCache(point, hrep)
+collision_cache(::Type{T}, hrep::HRep{M, 3, T}, point::GeometryTypes.Point{3, T}) where {M, T} = PointToHRepCollisionCache(hrep, point)
+
+reset!(cache::PointToHRepCollisionCache) = (cache.penetrated_halfspace_index[] = 0; return nothing)
+
+@inline function detect_contact(cache::PointToHRepCollisionCache{F, M}, pose_a::Transformation, pose_b::Transformation) where {F, M}
+    point = SVector(cache.point)
+    hrep = cache.hrep
+    flip = F
+
+    # Handle flipping of a and b
+    point_to_world, hrep_to_world = flip ? (pose_b, pose_a) : (pose_a, pose_b)
+    point_world = point_to_world(point)
+    point_hrep = inv(hrep_to_world)(point_world)
+
+    # Check all of the half-spaces (in hrep frame)
+    in_penetration = true
+    closest_index = 0
+    T = eltype(point) # TODO
+    closest_distance = typemax(T)
+    halfspaces = hrep.halfspaces
+    point_hrep_homogeneous = SVector(tuple(point_hrep[1], point_hrep[2], point_hrep[3], -one(eltype(point_hrep)))) # TODO: make nicer
+    @inbounds for i in 1 : M
+        halfspace = halfspaces[i]
+        normal = halfspace.outward_normal
+        sep = SVector(tuple(normal[1], normal[2], normal[3], halfspace.offset)) ⋅ point_hrep_homogeneous # TODO: make nicer
+        in_penetration &= sep < 0
+        distance = abs(sep)
+        if i === 1 || distance < closest_distance
+            closest_index = i
+            closest_distance = distance
+        end
+    end
+
+    if in_penetration
+        # Determine which half-space to use
+        if cache.penetrated_halfspace_index[] != 0
+            closest_index = cache.penetrated_halfspace_index[]
+        end
+    end
+
+    # Compute normal based on closest plane
+    @inbounds halfspace = halfspaces[closest_index]
+    rotation_to_world = LinearMap(transform_deriv(hrep_to_world, 0))
+    normal_world = rotation_to_world(halfspace.outward_normal)
+
+    # compute separation, closest point, caching
+    if in_penetration
+        separation = let normal = halfspace.outward_normal
+            SVector(tuple(normal[1], normal[2], normal[3], halfspace.offset)) ⋅ point_hrep_homogeneous
+        end
+        @assert separation < 0
+        closest_in_hrep = point_world
+        cache.penetrated_halfspace_index[] = closest_index
+    else
+        separation = closest_distance # kind of bogus...
+        closest_in_hrep = fill(NaN, typeof(point_world))
+        cache.penetrated_halfspace_index[] = 0
+    end
+
+    # Handle flipping of a and b and return
+    if flip
+        return separation, -normal_world, closest_in_hrep, point_world
+    else
+        return separation, normal_world, point_world, closest_in_hrep
+    end
 end
